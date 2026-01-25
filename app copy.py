c9e45b23a1917a55dac244428a744f8a90f2d5c5
maja-ja <134922426+maja-ja@users.noreply.github.com>
2026-01-25 14:03:23 +0800

    Update app copy.py

--- app copy.py
+++ app copy.py
@@ -163,7 +163,7 @@ def ui_domain_page(domain_data, title, theme_color, bg_color):
                 
                 with col_report:
                     # 呼叫新建立的回報組件
-                    ui_feedback_component(v['word'])
+                    ui_feedback_co７mponent(v['word'])
                 
                 # 這裡針對拆解 (breakdown) 使用金色與深色背景框
                 st.markdown(f"""
@@ -353,7 +353,7 @@ def main():
     st.sidebar.title("tymon Decoder")
     
     # 2. 導覽選單
-    menu = st.sidebar.radio("導航", ["字根區", "學習區", "高中 7000 區", "醫學區", "法律區", "人工智慧區", "心理與社會區", "生物與自然區", "管理區"])
+    menu = st.sidebar.radio("導航", ["字根區", "學習區", "國小區", "國中區", "高中區", "醫學區", "法律區", "人工智慧區", "心理與社會區", "生物與自然區", "管理區"])
     
     st.sidebar.divider()
     
